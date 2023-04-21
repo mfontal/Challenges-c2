3ec811eb047c62a105b21d7fd7f82a44c572ac61
mfontal <fontalmilton@gmail.com>
2023-04-21 12:10:33 -0500

    Subida del primer challenge del corte 2

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Padre } from './challenge-8/Padre'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Padre />
+  </React.StrictMode>,
+)
+
